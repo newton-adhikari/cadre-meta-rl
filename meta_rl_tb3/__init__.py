46f178c19231b0f01e33e6c48b0347e0d0dcda8e
Newton Adhikari <newtonadk@gmail.com>
2026-09-17 11:38:54 +0545

    created project skeleton #1

---
+++ meta_rl_tb3/__init__.py
@@ -0,0 +1,8 @@
+"""Meta-RL for TurtleBot3 Navigation.
+
+A Meta-Reinforcement Learning framework for adaptation to
+navigation tasks using TurtleBot3 in Gazebo simulation.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Newton Adhikari"
